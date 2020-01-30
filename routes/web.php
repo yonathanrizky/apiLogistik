310d2513aed298fcc451f0da3b56f53d81d779d3
Yonathan Rizky <1411510926@student.budiluhur.ac.id>
2020-01-30 17:12:45 +0700

    Validation & Login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-
 $router->group(['middleware' => 'auth'], function() use($router) {
     $router->get('/users', 'UserController@index');
     $router->post('/users', 'UserController@store');
